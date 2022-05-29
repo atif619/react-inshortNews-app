e95f1102453cdef421b48b13bab802d602bb986b
atif619 <sheikhatib39@gmail.com>
2022-05-29 15:57:18 +0530

    second comment

--- src/App.js
+++ src/App.js
@@ -11,13 +11,13 @@ function App() {
   const [menu, setMenu] = useState(false)
   const [loadmore, setLoadMore] = useState(20)
 
-  const API_KEY = '0730c4f7fcb04fb0b2958f59b4a89a8e';
+  const API_KEY = '18c0b0149a4d440b8da40ac9719a177a';
 
   const getNews = async () => {
     let res = await fetch(`https://newsapi.org/v2/top-headlines?country=in&category=${category}&pageSize=${loadmore}&apiKey=${API_KEY}`)
 
     let data = await res.json();
-    setNewsData(data.articles) 
+    setNewsData(data.articles)
     setTotalNewsResult(data.totalResults)
     // console.log(totalNewsResult);
   }
